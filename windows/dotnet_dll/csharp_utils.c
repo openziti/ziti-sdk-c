e7518c4a483a61cf062962e7e8f6b598f29a9112
Clint <clint@netfoundry.io>
2019-10-10 15:19:09 -0400

    add the windows dll project into the mix

---
+++ windows/dotnet_dll/csharp_utils.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+#include <nf/ziti.h>
+#include <stdlib.h>
+#include <uv.h>
+
+#define DIE(v) do { \
+int code = (v);\
+if (code != ZITI_OK) {\
+fprintf(stderr, "ERROR: " #v " => %s\n", ziti_errorstr(code));\
+exit(code);\
+}} while(0)
+
+uv_timer_t* registerUVTimerC(uv_loop_t* loop, uv_timer_cb timer_cb, uint64_t iterations, uint64_t delay) {
+	
+	uv_timer_t* uvt = calloc(1, sizeof(uv_timer_t));
+	uv_timer_init(loop, uvt);
+	uv_timer_start(uvt, timer_cb, iterations, delay);
+	return uvt;
+}
---
+++ windows/dotnet_dll/includes/dotnet_dll.h
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <nf/ziti.h>
+#include <zt_internal.h>
+#include <uv.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
