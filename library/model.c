e2be4225736782101aab208519ac9c29c4e2bbf9
Eugene K <eugene.kobyakov@netfoundry.io>
2019-10-22 17:22:39 -0400

    fix for mjson changes

--- library/model.c
+++ library/model.c
@@ -13,8 +13,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
+#define MJSON_API_ONLY
 #include <mjson.h>
+
+
 #include <stdbool.h>
 #include "model.h"
 
