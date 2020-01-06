c2e0e2a3219f3699fe9c614d987c1b6aaf76a5d2
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-01-06 14:30:46 -0500

    update copyrights to be in sync with 2020

--- library/controller.c
+++ library/controller.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 Netfoundry, Inc.
+Copyright 2019-2020 Netfoundry, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -402,22 +402,6 @@ int ziti_ctrl_process(struct nf_ctx* ctx, ...) {
     struct addrinfo *address, *ap;
     int r = getaddrinfo(ctx->controller, port, &hints, &address);
     if (r != 0 || address == NULL) {
-        /*
-        Copyright 2019 Netfoundry, Inc.
-
-        Licensed under the Apache License, Version 2.0 (the "License");
-        you may not use this file except in compliance with the License.
-        You may obtain a copy of the License at
-
-        https://www.apache.org/licenses/LICENSE-2.0
-
-        Unless required by applicable law or agreed to in writing, software
-        distributed under the License is distributed on an "AS IS" BASIS,
-        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-        See the License for the specific language governing permissions and
-        limitations under the License.
-        */
-
         ZITI_LOG(ERROR, "failed to resolve controller(%s): %s", ctx->controller, strerror(errno));
         return ZITI_CONTROLLER_UNAVAILABLE;
     }
