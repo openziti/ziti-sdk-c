4fae18386661068ce8c9e7ecd286cfc42382b005
Eugene K <eugene.kobyakov@netfoundry.io>
2020-03-04 11:27:50 -0500

    add libsodium depenency

---
+++ deps/libsodium.cmake
@@ -0,0 +1,32 @@
+include(FetchContent)
+
+FetchContent_Declare (
+        libsodium
+        GIT_REPOSITORY http://github.com/jedisct1/libsodium
+        GIT_TAG stable
+)
+
+FetchContent_GetProperties(libsodium)
+
+if(NOT libsodium_POPULATED)
+    FetchContent_Populate(libsodium)
+
+    if(NOT EXISTS ${libsodium_BINARY_DIR}/config.status)
+        execute_process(
+                COMMAND "${libsodium_SOURCE_DIR}/configure" "--prefix=${libsodium_BINARY_DIR}" --enable-debug
+                WORKING_DIRECTORY ${libsodium_BINARY_DIR}
+        )
+    endif()
+    execute_process(
+            COMMAND make
+            WORKING_DIRECTORY ${libsodium_BINARY_DIR}
+    )
+    execute_process(
+            COMMAND make install
+            WORKING_DIRECTORY ${libsodium_BINARY_DIR}
+    )
+endif()
+
+add_library(sodium IMPORTED STATIC GLOBAL)
+target_include_directories(sodium INTERFACE ${libsodium_BINARY_DIR}/include)
+set_target_properties(sodium PROPERTIES IMPORTED_LOCATION ${libsodium_BINARY_DIR}/lib/libsodium.a)
