7051373d7277a3f4c369f122c92b701641f3ffe7
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-04-08 08:40:43 -0400

    small tweaks to get libsodium to almost/kinda work (#69)
    * use pre-build libsodium for windows builds
    * use mingw/msvc binaries depending on generator
    
    Co-authored-by: Eugene K <eugene.kobyakov@netfoundry.io>

--- deps/libsodium.cmake
+++ deps/libsodium.cmake
@@ -1,10 +1,34 @@
 include(FetchContent)
 
 if (WIN32)
-FetchContent_Declare (
-        libsodium
-        URL https://download.libsodium.org/libsodium/releases/libsodium-1.0.18-stable-mingw.tar.gz
-    )
+
+    if(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+           FetchContent_Declare (
+		   libsodium
+		   URL	https://download.libsodium.org/libsodium/releases/libsodium-1.0.18-stable-msvc.zip
+		   )
+           if(CMAKE_EXE_LINKER_FLAGS MATCHES "/machine:x64")
+              set(arch "x64")
+           else()
+              set(arch "Win32")
+           endif()
+           if(CMAKE_BUILD_TYPE)
+              set(build_type ${CMAKE_BUILD_TYPE})
+           else()
+              set(build_type "Debug")
+           endif()
+
+           set(libsodium_include_path include)
+           set(libsodium_lib_path ${arch}/${build_type}/v${MSVC_TOOLSET_VERSION}/static/libsodium${CMAKE_STATIC_LIBRARY_SUFFIX})
+           message("libsodium = ${libsodium_lib_path}")
+    else()
+           FetchContent_Declare (
+		   libsodium
+		   URL	https://download.libsodium.org/libsodium/releases/libsodium-1.0.18-stable-mingw.tar.gz
+		   )
+           set(libsodium_include_path libsodium-win64/include)
+           set(libsodium_lib_path libsodium-win64/lib/libsodium.a)
+	endif()
 else()
 FetchContent_Declare (
         libsodium
@@ -17,11 +41,10 @@ FetchContent_GetProperties(libsodium)
 
 if(NOT libsodium_POPULATED)
     FetchContent_Populate(libsodium)
+    if (NOT EXISTS ${libsodium_BINARY_DIR})
+    	file(MAKE_DIRECTORY	${libsodium_BINARY_DIR})
+    endif ()
     if (NOT WIN32)
-        if (NOT EXISTS ${libsodium_BINARY_DIR})
-            file(MAKE_DIRECTORY ${libsodium_BINARY_DIR})
-        endif ()
-
         if (NOT EXISTS ${libsodium_BINARY_DIR}/config.status)
             # first build on macos fails because CMake picks up xcode
             unset(ENV{CC})
@@ -30,8 +53,8 @@ if(NOT libsodium_POPULATED)
                     COMMAND "${libsodium_SOURCE_DIR}/configure" "--prefix=${libsodium_BINARY_DIR}" --enable-debug --with-pic --host=${triple}
                     WORKING_DIRECTORY ${libsodium_BINARY_DIR}
             )
-        endif ()
-        execute_process(
+        endif()
+		execute_process(
                 COMMAND make
                 WORKING_DIRECTORY ${libsodium_BINARY_DIR}
         )
@@ -39,17 +62,18 @@ if(NOT libsodium_POPULATED)
                 COMMAND make install
                 WORKING_DIRECTORY ${libsodium_BINARY_DIR}
         )
-    endif()
-endif()
+    endif ()
+endif ()
 
 add_library(sodium IMPORTED STATIC GLOBAL)
 
 if (WIN32)
-    target_include_directories(sodium INTERFACE ${libsodium_SOURCE_DIR}/libsodium-win64/include)
+    target_include_directories(sodium INTERFACE ${libsodium_SOURCE_DIR}/${libsodium_include_path})
     target_compile_definitions(sodium INTERFACE SODIUM_STATIC)
-    target_link_directories(sodium INTERFACE ${libsodium_SOURCE_DIR}/libsodium-win64/lib)
-    set_target_properties(sodium PROPERTIES IMPORTED_LOCATION ${libsodium_SOURCE_DIR}/libsodium-win64/lib/libsodium.a)
+    #target_link_directories(sodium INTERFACE ${libsodium_SOURCE_DIR})
+	set_target_properties(sodium PROPERTIES	IMPORTED_LOCATION ${libsodium_SOURCE_DIR}/${libsodium_lib_path})
 else()
     target_include_directories(sodium INTERFACE ${libsodium_BINARY_DIR}/include)
     set_target_properties(sodium PROPERTIES IMPORTED_LOCATION ${libsodium_BINARY_DIR}/lib/libsodium.a)
 endif()
+
