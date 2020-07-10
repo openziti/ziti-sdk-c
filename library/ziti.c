2be4349ddb040266b7ada93216a1f3d98085f0c6
ekoby <7406535+ekoby@users.noreply.github.com>
2020-07-10 08:57:56 -0400

    re-auth if SDK gets UNAUTHORIZED from controller (#124)
    * reath if SDK gets UNAUTHORIZED from controller
    * proper clean up on de-auth/de-enrollment
      * clear sessions if api-session was de-authorized
      * notify app that services are no longer available on de-enrollment

--- library/ziti.c
+++ library/ziti.c
@@ -458,9 +458,26 @@ static void session_refresh(uv_timer_t *t) {
     ziti_ctrl_current_api_session(&ztx->controller, session_cb, req);
 }
 
+static void ziti_re_auth(ziti_context ztx) {
+    ZITI_LOG(WARN, "starting to re-auth");
+    uv_timer_stop(&ztx->refresh_timer);
+    uv_timer_stop(&ztx->session_timer);
+    free_ziti_session(ztx->session);
+    FREE(ztx->session);
+    model_map_clear(&ztx->sessions, (_free_f) free_ziti_net_session);
+
+    NEWP(init_req, struct ziti_init_req);
+    init_req->ztx = ztx;
+    ziti_ctrl_login(&ztx->controller, ztx->opts->config_types, session_cb, init_req);
+}
+
 static void update_services(ziti_service_array services, ziti_error *error, ziti_context ztx) {
     if (error) {
         ZITI_LOG(ERROR, "failed to get service updates err[%s/%s]", error->code, error->message);
+        if (strcmp(error->code, "UNAUTHORIZED") == 0) {
+            ZITI_LOG(WARN, "API session is no longer valid. Trying to re-auth");
+            ziti_re_auth(ztx);
+        }
         return;
     }
 
@@ -579,7 +596,26 @@ static void session_cb(ziti_session *session, ziti_error *err, void *ctx) {
         }
 
     } else {
-        ZITI_LOG(ERROR, "failed to login: %s[%d](%s)", err->code, errCode, err->message);
+        if (ztx->session) {
+            ZITI_LOG(WARN, "failed to refresh: %s[%d]", err->code, errCode);
+            if (strcmp(err->code, "UNAUTHORIZED") == 0) {
+                ziti_re_auth(ztx);
+            } else {
+                uv_timer_start(&ztx->session_timer, session_refresh, 5 * 1000, 0);
+            }
+        } else {
+            ZITI_LOG(ERROR, "failed to login: %s[%d](%s)", err->code, errCode, err->message);
+
+            if (ztx->opts->service_cb) {
+                const char *name;
+                ziti_service *srv;
+                MODEL_MAP_FOREACH(name, srv, ztx->services) {
+                    ztx->opts->service_cb(ztx, srv, ZITI_SERVICE_UNAVAILABLE, ztx->opts->ctx);
+                }
+            }
+
+            model_map_clear(&ztx->services, free_ziti_service);
+        }
     }
 
     if (init_req->init_cb) {
