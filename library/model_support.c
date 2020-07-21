54f9f811c0c08897b3f38b1849634edc66bb4436
eugene <eugene.kobyakov@netfoundry.io>
2020-07-21 01:49:32 -0400

    fix gmtime on WIN32

--- library/model_support.c
+++ library/model_support.c
@@ -660,11 +660,16 @@ static int _json_json(const char *s, int indent, char *json, size_t max, size_t 
 }
 
 static int _timeval_json(timestamp *t, int indent, char *json, size_t max, size_t *len) {
-    struct tm *tm2 = gmtime(&t->tv_sec);
+    struct tm tm2;
+#if _WIN32
+    _gmtime32_s(&tm2, &t->tv_sec);
+#else
+    gmtime_r(&t->tv_sec, &tm2);
+#endif
 
     int rc = snprintf(json, max, "\"%04d-%02d-%02dT%02d:%02d:%02d.%06ldZ\"",
-                      tm2->tm_year + 1900, tm2->tm_mon + 1, tm2->tm_mday,
-                      tm2->tm_hour, tm2->tm_min, tm2->tm_sec, t->tv_usec);
+                      tm2.tm_year + 1900, tm2.tm_mon + 1, tm2.tm_mday,
+                      tm2.tm_hour, tm2.tm_min, tm2.tm_sec, t->tv_usec);
     *len = rc;
     return 0;
 }
