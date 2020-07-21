4bcc45b957404112f820c68dcc6d38ebb1dbf364
Eugene K <eugene.kobyakov@netfoundry.io>
2020-07-21 14:21:34 -0400

    cleanup

--- library/jwt.c
+++ library/jwt.c
@@ -20,96 +20,7 @@ limitations under the License.
 #include "internal_model.h"
 #include "ziti_enroll.h"
 
-
 const char* ZITI_SDK_JWT_FILE = "ZITI_SDK_JWT_FILE";
-
-static char* url64to64(const char* in, size_t ilen, size_t *olen) {
-    size_t size = ((ilen - 1)/4 + 1) * 4;
-
-    char *out = (char*)calloc(1, size + 1);
-    size_t i;
-    for(i = 0; i < ilen; i++) {
-        switch (in[i]) {
-            case '_': out[i] = '/'; break;
-            case '-': out[i] = '+'; break;
-            default: out[i] = in[i];
-        }
-    }
-
-    while(i < size) {
-        out[i++] = '=';
-    }
-    *olen = size;
-    return out;
-}
-
-static const unsigned char pr2six[256] = {
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 63,
-    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64,
-    64,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 63,
-    64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64
-};
-
-static int base64url_decode_len(const char *bufcoded) {
-    int nbytesdecoded;
-    register const unsigned char *bufin;
-    register int nprbytes;
-
-    bufin = (const unsigned char *) bufcoded;
-    while (pr2six[*(bufin++)] <= 63);
-
-    nprbytes = (bufin - (const unsigned char *) bufcoded) - 1;
-    nbytesdecoded = ((nprbytes + 3) / 4) * 3;
-
-    return nbytesdecoded;
-}
-
-static int base64url_decode(char *bufplain, const char *bufcoded) {
-    register const unsigned char *bufin;
-    register unsigned char *bufout;
-    register int nprbytes;
-
-    bufin = (const unsigned char *) bufcoded;
-    while (pr2six[*(bufin++)] <= 63);
-    nprbytes = (bufin - (const unsigned char *) bufcoded) - 1;
-
-    bufout = (unsigned char *) bufplain;
-    bufin = (const unsigned char *) bufcoded;
-
-    while (nprbytes > 4) {
-        *(bufout++) = (unsigned char) (pr2six[*bufin] << 2 | pr2six[bufin[1]] >> 4);
-        *(bufout++) = (unsigned char) (pr2six[bufin[1]] << 4 | pr2six[bufin[2]] >> 2);
-        *(bufout++) = (unsigned char) (pr2six[bufin[2]] << 6 | pr2six[bufin[3]]);
-        bufin += 4;
-        nprbytes -= 4;
-    }
-
-    if (nprbytes > 1)
-        *(bufout++) = (unsigned char) (pr2six[*bufin] << 2 | pr2six[bufin[1]] >> 4);
-    if (nprbytes > 2)
-        *(bufout++) = (unsigned char) (pr2six[bufin[1]] << 4 | pr2six[bufin[2]] >> 2);
-    if (nprbytes > 3)
-        *(bufout++) = (unsigned char) (pr2six[bufin[2]] << 6 | pr2six[bufin[3]]);
-
-    size_t len = ((char*)bufout - bufplain);
-
-    ZITI_LOG(DEBUG, "base64url_decode len is: %zu", len);
-
-    return len;
-}
-
 
 int load_jwt_file(const char *filename, struct enroll_cfg_s *ecfg, ziti_enrollment_jwt_header **zejh, ziti_enrollment_jwt **zej) {
     ZITI_LOG(DEBUG, "filename is: %s", filename);
@@ -146,29 +57,31 @@ int load_jwt_file(const char *filename, struct enroll_cfg_s *ecfg, ziti_enrollme
     ecfg->jwt_signing_input = (unsigned char *) calloc(1, strlen(ecfg->raw_jwt) + 1);
     strncpy((char *) ecfg->jwt_signing_input, ecfg->raw_jwt, (dot2 - ecfg->raw_jwt));
     ZITI_LOG(DEBUG, "ecfg->jwt_signing_input is: \n%s", ecfg->jwt_signing_input);
-    ecfg->jwt_sig = calloc(1, sizeof(char) * (base64url_decode_len(dot2 + 1)));
-    ecfg->jwt_sig_len = base64url_decode(ecfg->jwt_sig, dot2 + 1);
-    ZITI_LOG(DEBUG, "sig is: \n%s", dot2 + 1);
+    um_base64url_decode(dot2 + 1, &ecfg->jwt_sig, &ecfg->jwt_sig_len);
 
-    char *buf = malloc(dot2 - ecfg->raw_jwt); // that's big enough
+    size_t header_len;
+    char *header;
+    um_base64url_decode(ecfg->raw_jwt, &header, &header_len);
 
-    size_t header_len = base64url_decode(buf, ecfg->raw_jwt);
     *zejh = calloc(1, sizeof(ziti_enrollment_jwt_header));
-    if (parse_ziti_enrollment_jwt_header(*zejh, buf, header_len) != 0) {
+    if (parse_ziti_enrollment_jwt_header(*zejh, header, header_len) != 0) {
         free_ziti_enrollment_jwt_header(*zejh);
         FREE(*zejh);
         return ZITI_JWT_INVALID_FORMAT;
     }
+    free(header);
 
-    size_t blen = base64url_decode(buf, dot1 + 1);
+    size_t blen;
+    char *body;
+    um_base64url_decode(dot1 + 1, &body, &blen);
 
     *zej = calloc(1, sizeof(ziti_enrollment_jwt));
-    if (parse_ziti_enrollment_jwt(*zej, buf, blen) != 0) {
+    if (parse_ziti_enrollment_jwt(*zej, body, blen) != 0) {
         free_ziti_enrollment_jwt(*zej);
         FREE(*zej);
         return ZITI_JWT_INVALID_FORMAT;
     }
-    free(buf);
+    free(body);
     return ZITI_OK;
 }
 
