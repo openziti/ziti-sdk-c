4ad99c58f1e83d4f339aaa718ca59eca2f9df81f
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-08-28 12:04:13 -0400

    extract just the errors into a separate file

--- includes/ziti/errors.h
+++ includes/ziti/errors.h
@@ -26,70 +26,7 @@ limitations under the License.
 // @endcond
 
 #include "externs.h"
- /**
-  * A macro defining the various errors conditions expected to be seen
-  * when using the C SDK
-  */
-#define ZITI_ERRORS(XX) \
-    /** The expected outcome of a successful operation */ \
-    XX(OK, "OK") \
-    /** The provided configuration was not found */ \
-    XX(CONFIG_NOT_FOUND, "Configuration not found") \
-    /** The provided JWT was not found */ \
-    XX(JWT_NOT_FOUND, "JWT not found") \
-    /** The provided JWT is not accepted by controller */ \
-    XX(JWT_INVALID, "JWT not accepted by controller") \
-    /** The provided JWT has invalid format */ \
-    XX(JWT_INVALID_FORMAT, "JWT has invalid format") \
-    /** PKCS7/ASN.1 parsing failed */ \
-    XX(PKCS7_ASN1_PARSING_FAILED, "PKCS7/ASN.1 parsing failed") \
-    /** unsupported JWT signing algorithm */ \
-    XX(JWT_SIGNING_ALG_UNSUPPORTED, "unsupported JWT signing algorithm") \
-    /** JWT verification failed */ \
-    XX(JWT_VERIFICATION_FAILED, "JWT verification failed") \
-    /** unsupported enrollment method */ \
-    XX(ENROLLMENT_METHOD_UNSUPPORTED, "unsupported enrollment method") \
-    /** enrollment method requires client certificate */ \
-    XX(ENROLLMENT_CERTIFICATE_REQUIRED, "enrollment method requires certificate") \
-    /** Attempt to generate an private key failed */ \
-    XX(KEY_GENERATION_FAILED, "error generating private key") \
-    /** Attempt to load TLS key failed */ \
-    XX(KEY_LOAD_FAILED, "error loading TLS key") \
-    /** Attempt to generate a CSR failed */ \
-    XX(CSR_GENERATION_FAILED, "error generating a CSR") \
-    /** Some or all of the provided configuration is incorrect */ \
-    XX(INVALID_CONFIG, "Configuration is invalid") \
-    /** Returned when the identity does not have the correct level of access needed.
-    Common causes are:
-    * no policy exists granting the identity access
-    * the certificates presented are incorrect, out of date, or invalid
-    */ \
-    XX(NOT_AUTHORIZED, "Not Authorized") \
-    /** The SDK has attempted to communicate to the Ziti Controller but the controller
-    is offline or did not respond to the request*/ \
-    XX(CONTROLLER_UNAVAILABLE, "Ziti Controller is not available") \
-    /** The SDK cannot send data to the Ziti Network because an Edge Router was not available. Common causes are:
-    * the identity connecting is not associated with any Edge Routers
-    * the Edge Router in use is no longer responding */ \
-    XX(GATEWAY_UNAVAILABLE, "Ziti Edge Router is not available") \
-    /** The SDK cannot send data to the Ziti Network because the requested service was not available. Common causes are:
-    * the service does not exist
-    * the identity connecting is not associated with the given service
-    */ \
-    XX(SERVICE_UNAVAILABLE, "Service not available") \
-    /** The connection has been closed gracefully */ \
-    XX(EOF, "Connection closed") \
-    /** A connect or write operation did not complete in the alloted timeout. #DEFAULT_TIMEOUT */ \
-    XX(TIMEOUT, "Operation did not complete in time") \
-    /** The connection has been closed abnormally. */ \
-    XX(CONNABORT, "Connection to edge router terminated") \
-    /** SDK detected invalid state, most likely caaused by improper use. */ \
-    XX(INVALID_STATE, "invalid state") \
-    /** SDK detected invalid cryptographic state of Ziti connection */ \
-    XX(CRYPTO_FAIL, "crypto failure") \
-    /** Inspired by the Android SDK: What a Terrible Failure. A condition that should never happen. */ \
-    XX(WTF, "WTF: programming error")
-
+#include "error-defs.h"
 
 #ifdef __cplusplus
 extern "C" {
