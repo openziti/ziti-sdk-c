8fbb6995d924d93808963c0e97e68825401df61f
Eugene K <eugene.kobyakov@netfoundry.io>
2022-04-29 14:34:14 -0400

    misc crash fixes

--- library/connect.c
+++ library/connect.c
@@ -1,18 +1,16 @@
-/*
-Copyright 2019-2020 NetFoundry, Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright (c) 2022.  NetFoundry, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <stdlib.h>
 #include <posture.h>
@@ -803,7 +801,9 @@ static bool flush_to_client(ziti_connection conn) {
     }
 
     if (conn->state == Disconnected) {
-        conn->data_cb(conn, NULL, ZITI_CONN_CLOSED);
+        if (conn->data_cb) {
+            conn->data_cb(conn, NULL, ZITI_CONN_CLOSED);
+        }
     }
     return false;
 }
