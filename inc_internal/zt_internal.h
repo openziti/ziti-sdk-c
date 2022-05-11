8c82ecca4a0aede24d2864c6686d4c569084665d
Eugene K <eugene.kobyakov@netfoundry.io>
2022-05-11 16:07:52 -0400

    simplify outbound FIN/ziti_close_write() handling

--- inc_internal/zt_internal.h
+++ inc_internal/zt_internal.h
@@ -1,18 +1,16 @@
-/*
-Copyright 2019-2020 NetFoundry, Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright (c) 2022.  NetFoundry, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef ZT_SDK_ZT_INTERNAL_H
 #define ZT_SDK_ZT_INTERNAL_H
@@ -114,6 +112,7 @@ struct ziti_write_req_s {
     struct ziti_conn *conn;
     uint8_t *buf;
     size_t len;
+    bool eof;
 
     uint8_t *payload; // internal buffer
     ziti_write_cb cb;
