6404ef19b96c248ee81b6fb0f67be56f49003482
Shawn Carey <shawn.carey@netfoundry.io>
2023-04-01 02:04:45 +0000

    regenerate msvc projects in libsodium after fetching

--- deps/libsodium.cmake
+++ deps/libsodium.cmake
@@ -84,6 +84,7 @@ if(NOT libsodium_POPULATED)
         execute_process(
                 COMMAND msbuild ${libsodium_SOURCE_DIR}/builds/msvc/vs2022/libsodium.sln -property:Configuration=StaticDebug -property:Platform=${arch}
                 WORKING_DIRECTORY ${libsodium_BINARY_DIR}
+                COMMAND regen-msvc/regen-msvc.py
         )
     endif ()
 endif ()
