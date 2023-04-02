da31285aadb1e740dc94cbfdb6d112bcf88dc702
Shawn Carey <shawn.carey@netfoundry.io>
2023-04-02 17:14:53 +0000

    comments

--- toolchains/Windows-ARM64-msvc.cmake
+++ toolchains/Windows-ARM64-msvc.cmake
@@ -1,7 +1,8 @@
+# cross-compile for windows/arm64 on windows/x64 host with visual studio
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR ARM64)
+# setting CMAKE_GENERATOR_PLATFORM should be sufficient if you believe the doc, it results in build files
+# that cause msbuild to that the ZERO_CHECK project doesn't contain the "Debug|x64" platform/config
+# combination. running cmake with '-A ARCH64' avoids the msbuild failure.
 set(CMAKE_GENERATOR_PLATFORM ARM64)
-# from VS command prompt, run `vsdevcmd -arch=arm64 -host_arch=amd64 -winsdk=10.0.19041.0`
-# C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\HostX64\ARM64
-#set(VCINSTALLDIR "C:/Program Files/Microsoft Visual Studio/2022/Community/VC")
 set(CMAKE_C_COMPILER cl.exe)
