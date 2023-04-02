c9ca9cc84d09eb4fc56008b9e4df250d9aaf84c8
Shawn Carey <shawn.carey@netfoundry.io>
2023-04-02 18:19:50 +0000

    change conditionals to build libsodium when the source was fetched, instead of inferring based on compiler/platform

--- deps/libsodium.cmake
+++ deps/libsodium.cmake
@@ -1,35 +1,27 @@
 include(FetchContent)
 
 if (WIN32)
-
     if(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
-        if (NOT CMAKE_EXE_LINKER_FLAGS MATCHES "/machine:ARM64")
+        if(CMAKE_EXE_LINKER_FLAGS MATCHES "/machine:x64")
+            set(arch "x64")
+        else()
+            set(arch "Win32")
+        endif()
+        if(CMAKE_BUILD_TYPE)
+            set(build_type ${CMAKE_BUILD_TYPE})
+        else()
+            set(build_type "Debug")
+        endif()
+        if (NOT CMAKE_SYSTEM_PROCESSOR STREQUAL "ARM64")
             FetchContent_Declare (
                     libsodium
                     URL	https://download.libsodium.org/libsodium/releases/libsodium-1.0.18-stable-msvc.zip
             )
-            if(CMAKE_EXE_LINKER_FLAGS MATCHES "/machine:x64")
-                set(arch "x64")
-            else()
-                set(arch "Win32")
-            endif()
-            if(CMAKE_BUILD_TYPE)
-                set(build_type ${CMAKE_BUILD_TYPE})
-            else()
-                set(build_type "Debug")
-            endif()
-
             set(libsodium_include_path include)
             set(libsodium_lib_path ${arch}/${build_type}/v${MSVC_TOOLSET_VERSION}/static/libsodium${CMAKE_STATIC_LIBRARY_SUFFIX})
-            message("libsodium = ${libsodium_lib_path}")
         else()
             # arm builds are not included in libsodium release tarballs (yet?), so we need to build it.
-            # windows/arm build support has been added to master, but not stable (yet?) (https://github.com/jedisct1/libsodium/pull/1130)
-            if(CMAKE_BUILD_TYPE)
-                set(build_type ${CMAKE_BUILD_TYPE})
-            else()
-                set(build_type "Debug")
-            endif()
+            # windows/arm build support has been added to master, but not stable. See https://github.com/jedisct1/libsodium/pull/1130
             set(arch "ARM64")
             FetchContent_Declare (
                     libsodium
@@ -47,6 +39,7 @@ if (WIN32)
         set(libsodium_include_path libsodium-win64/include)
         set(libsodium_lib_path libsodium-win64/lib/libsodium.a)
     endif()
+    message("libsodium = ${libsodium_lib_path}")
 else()
     FetchContent_Declare (
             libsodium
@@ -62,36 +55,45 @@ if(NOT libsodium_POPULATED)
     if (NOT EXISTS ${libsodium_BINARY_DIR})
         file(MAKE_DIRECTORY	${libsodium_BINARY_DIR})
     endif ()
-    if (NOT WIN32)
+    # perform the build if the source was fetched.
+    if (EXISTS ${libsodium_SOURCE_DIR}/src)
         if (NOT EXISTS ${libsodium_BINARY_DIR}/config.status)
-            # first build on macos fails because CMake picks up xcode
-            if (APPLE)
-                unset(ENV{CC})
-                unset(ENV{CXX})
+            if (NOT CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+                # first build on macos fails because CMake picks up xcode
+                if (APPLE)
+                    unset(ENV{CC})
+                    unset(ENV{CXX})
+                endif()
+                execute_process(
+                        COMMAND "${libsodium_SOURCE_DIR}/configure" "--prefix=${libsodium_BINARY_DIR}"
+                        --disable-opt --without-pthreads --with-pic --host=${triple}
+                        --with-sysroot=${CMAKE_SYSROOT}
+                        COMMAND_ECHO STDOUT
+                        COMMAND_ERROR_IS_FATAL ANY
+                        WORKING_DIRECTORY ${libsodium_BINARY_DIR}
+                )
+                execute_process(
+                        COMMAND make -j4
+                        COMMAND_ECHO STDOUT
+                        COMMAND_ERROR_IS_FATAL ANY
+                        WORKING_DIRECTORY ${libsodium_BINARY_DIR}
+                )
+                execute_process(
+                        COMMAND make install
+                        COMMAND_ECHO STDOUT
+                        COMMAND_ERROR_IS_FATAL ANY
+                        WORKING_DIRECTORY ${libsodium_BINARY_DIR}
+                )
+            else()
+                execute_process(
+                        COMMAND msbuild ${libsodium_SOURCE_DIR}/builds/msvc/vs2019/libsodium.sln -property:Configuration=StaticDebug -property:Platform=${arch}
+                        COMMAND_ECHO STDOUT
+                        COMMAND_ERROR_IS_FATAL ANY
+                        WORKING_DIRECTORY ${libsodium_BINARY_DIR}
+                )
             endif()
-            execute_process(
-                    COMMAND "${libsodium_SOURCE_DIR}/configure" "--prefix=${libsodium_BINARY_DIR}"
-                    --disable-opt --without-pthreads --with-pic --host=${triple}
-                    --with-sysroot=${CMAKE_SYSROOT}
-                    WORKING_DIRECTORY ${libsodium_BINARY_DIR}
-            )
         endif()
-        execute_process(
-                COMMAND make -j4
-                WORKING_DIRECTORY ${libsodium_BINARY_DIR}
-        )
-        execute_process(
-                COMMAND make install
-                WORKING_DIRECTORY ${libsodium_BINARY_DIR}
-        )
-    else()
-        execute_process(
-                COMMAND msbuild ${libsodium_SOURCE_DIR}/builds/msvc/vs2019/libsodium.sln -property:Configuration=StaticDebug -property:Platform=${arch}
-                COMMAND_ECHO STDOUT
-                COMMAND_ERROR_IS_FATAL ANY
-                WORKING_DIRECTORY ${libsodium_SOURCE_DIR}
-        )
-    endif ()
+    endif()
 endif ()
 
 add_library(sodium IMPORTED STATIC GLOBAL)
