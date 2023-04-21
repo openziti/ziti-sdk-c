7c0b1e37ba7f0241edabbc832fd4bac352e84732
Eugene K <eugene.kobyakov@netfoundry.io>
2023-04-21 13:27:18 -0400

    make sure GIT_BRANCH and GIT_COMMIT_HASH are populated

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,5 +1,5 @@
 
-function(get_version version_file version_var)
+function(get_version version_file version_var branch_var commit_var)
     execute_process(
             COMMAND git describe --tags HEAD
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
@@ -8,12 +8,32 @@ function(get_version version_file version_var)
             ERROR_VARIABLE git_error
     )
 
+    if (NOT git_error)
+        execute_process(
+                COMMAND git rev-parse --abbrev-ref HEAD
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                OUTPUT_VARIABLE branch
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+
+        # Get the latest abbreviated commit hash of the working branch
+        execute_process(
+                COMMAND git log -1 --format=%h
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                OUTPUT_VARIABLE commit
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+
+    endif ()
+
     if (git_error)
         unset(GIT_INFO)
         message("getting version from ${version_file}")
         file(STRINGS ${version_file} ver_info)
         list(LENGTH ver_info ver_info_len)
         list(GET ver_info 0 GIT_INFO)
+        list(GET ver_info 1 branch)
+        list(GET ver_info 2 commit)
     endif ()
 
     if (${GIT_INFO} MATCHES "^v?([0-9]+\\.[0-9]+\\.[0-9]+)$")
@@ -23,4 +43,6 @@ function(get_version version_file version_var)
     endif ()
 
     set(${version_var} ${_ver} PARENT_SCOPE)
+    set(${branch_var} ${branch} PARENT_SCOPE)
+    set(${commit_var} ${commit} PARENT_SCOPE)
 endfunction()
