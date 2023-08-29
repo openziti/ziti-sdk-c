3adee27f31ebcafb3d85abb4e876a879fc7701e9
Eugene K <eugene.kobyakov@netfoundry.io>
2023-08-29 13:46:02 -0400

    add new CA bundle to API event

--- includes/ziti/ziti_events.h
+++ includes/ziti/ziti_events.h
@@ -1,18 +1,16 @@
-/*
-Copyright (c) 2020 NetFoundry, Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright (c) 2020-2023.  NetFoundry Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 
 #ifndef ZITI_SDK_ZITI_EVENTS_H
@@ -62,6 +60,7 @@ struct ziti_context_event {
 
 struct ziti_api_event {
     const char *new_ctrl_address;
+    const char *new_ca_bundle;
 };
 /**
  * \brief Edge Router Event.
