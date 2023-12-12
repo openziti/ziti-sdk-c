e37b7f33910466c52d91dc4b7823319d1c6f3857
Eugene K <eugene.kobyakov@netfoundry.io>
2023-12-12 15:13:19 -0500

    add missing protocol constants

--- inc_internal/edge_protocol.h
+++ inc_internal/edge_protocol.h
@@ -1,18 +1,16 @@
-/*
-Copyright (c) 2020 NetFoundry, Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright (c) 2020-2024. NetFoundry Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 
 #ifndef ZITI_SDK_EDGE_PROTOCOL_H
@@ -49,6 +47,14 @@ enum content_type {
     ContentTypeTraceRouteResponse  = 60797,
     ContentTypeConnInspectRequest  = 60798,
     ContentTypeConnInspectResponse = 60799,
+
+    ContentTypeUpdateTokenType = 60800,
+    ContentTypeUpdateTokenSuccessType = 60801,
+    ContentTypeUpdateTokenFailureType = 60802,
+
+    ContentTypePostureResponseType = 10800,
+    ContentTypePostureResponseSuccessType = 10801,
+
 };
 
 enum header_id {
