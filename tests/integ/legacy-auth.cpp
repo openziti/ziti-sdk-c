c09a8fd1fcc7328ac6f7890678f98c0684fb9960
ekoby <eugene.kobyakov@netfoundry.io>
2024-02-26 14:42:38 -0500

    auth integration test

--- tests/integ/legacy-auth.cpp
+++ tests/integ/legacy-auth.cpp
@@ -1,9 +1,9 @@
-// Copyright (c) 2019-2023.  NetFoundry Inc.
+// Copyright (c) 2019-2024. NetFoundry Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
 //
+// You may obtain a copy of the License at
 // https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,14 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "catch2_includes.hpp"
+#include "../catch2_includes.hpp"
 
 #include <iostream>
 #include <zt_internal.h>
 #include <ziti_ctrl.h>
 #include <utils.h>
 
-static const char *const SERVICE_NAME = "httpbin.ziti";
+#include "test-data.h"
+
+static const char *const SERVICE_NAME = TEST_SERVICE;
+
 using namespace std;
 using namespace Catch::Matchers;
 
@@ -125,11 +128,7 @@ TEST_CASE("invalid_controller", "[controller][GH-44]") {
 }
 
 TEST_CASE("controller_test","[integ]") {
-    char *conf = getenv("ZITI_SDK_CONFIG");
-    if (conf == nullptr) {
-        FAIL("ZITI_SDK_CONFIG environment variable is not set");
-        return;
-    }
+    const char *conf = TEST_CLIENT;
 
     ziti_config config;
     tls_context *tls;
@@ -160,24 +159,24 @@ TEST_CASE("controller_test","[integ]") {
     }
 
     WHEN("try to login and get non-existing service") {
-        auto session = do_login(ctrl);
+        auto api_sesh = do_login(ctrl);
 
         auto s = do_get1(ctrl, ziti_ctrl_get_service, "this-service-should-not-exist");
         THEN("should NOT get non-existent service") {
             REQUIRE(s == nullptr);
         }
-        free_ziti_api_session_ptr(session);
+        free_ziti_api_session_ptr(api_sesh);
     }
 
     WHEN("try to login, get service, and session") {
-        auto session = do_login(ctrl);
+        auto api_session = do_login(ctrl);
 
         auto services = do_get(ctrl, ziti_ctrl_get_services);
         ziti_service *s = services[0];
 
         THEN("should get service") {
             REQUIRE(s != nullptr);
-        }AND_THEN("should get session") {
+        }AND_THEN("should get api_session") {
             auto ns = do_get2(ctrl, ziti_ctrl_create_session, (const char *) s->id, *s->permissions[0]);
             REQUIRE(ns != nullptr);
             REQUIRE(ns->token != nullptr);
@@ -188,7 +187,7 @@ TEST_CASE("controller_test","[integ]") {
             do_get(ctrl, ziti_ctrl_logout);
         }
 
-        free_ziti_api_session_ptr(session);
+        free_ziti_api_session_ptr(api_session);
     }
 
     CATCH(ziti) {
@@ -203,4 +202,46 @@ TEST_CASE("controller_test","[integ]") {
     uv_run(loop, UV_RUN_DEFAULT);
     tls->free_ctx(tls);
     free_ziti_config(&config);
+}
+
+TEST_CASE("ztx-legacy-auth", "[integ]") {
+    const char *zid = TEST_CLIENT;
+
+    ziti_config cfg;
+    REQUIRE(ziti_load_config(&cfg, zid) == ZITI_OK);
+
+    ziti_context ztx;
+    REQUIRE(ziti_context_init(&ztx, &cfg) == ZITI_OK);
+
+    struct test_context_s {
+        int event;
+        std::string data;
+    } test_context = {
+        .event = 0,
+    };
+
+
+    ziti_options opts = {
+        .app_ctx = &test_context,
+        .events = ZitiContextEvent,
+        .event_cb = [](ziti_context ztx, const ziti_event_t *event){
+            printf("got event: %d => %s \n", event->type, event->event.ctx.err);
+            auto test_ctx = (test_context_s*)ziti_app_ctx(ztx);
+            test_ctx->event = event->type;
+        },
+    };
+
+    ziti_context_set_options(ztx, &opts);
+
+    auto l = uv_loop_new();
+    ziti_context_run(ztx, l);
+
+    while (test_context.event == 0) {
+        uv_run(l, UV_RUN_ONCE);
+    }
+
+    ziti_shutdown(ztx);
+    uv_run(l, UV_RUN_DEFAULT);
+
+    free_ziti_config(&cfg);
 }
