0a587201522e65ea09e77d799217985d90a10597
eugene <eugene.kobyakov@netfoundry.io>
2024-03-13 11:08:05 -0400

    flatten ziti_api_session

--- inc_internal/internal_model.h
+++ inc_internal/internal_model.h
@@ -49,10 +49,10 @@ XX(path, string, none, path, __VA_ARGS__)
 #define ZITI_API_SESSION_MODEL(XX, ...)\
 XX(id, string, none, id, __VA_ARGS__) \
 XX(token, string, none, token, __VA_ARGS__) \
-XX(expires, timestamp, ptr, expiresAt, __VA_ARGS__) \
-XX(expireSeconds, int, ptr, expirationSeconds, __VA_ARGS__) \
-XX(updated, timestamp, ptr, updatedAt, __VA_ARGS__) \
-XX(cached_last_activity_at, timestamp, ptr, cachedLastActivityAt, __VA_ARGS__) \
+XX(expires, timestamp, none, expiresAt, __VA_ARGS__) \
+XX(expireSeconds, int, none, expirationSeconds, __VA_ARGS__) \
+XX(updated, timestamp, none, updatedAt, __VA_ARGS__) \
+XX(cached_last_activity_at, timestamp, none, cachedLastActivityAt, __VA_ARGS__) \
 XX(identity, ziti_identity, ptr, identity, __VA_ARGS__) \
 XX(posture_query_set, ziti_posture_query_set, array, postureQueries, __VA_ARGS__) \
 XX(auth_queries, ziti_auth_query_mfa, list, authQueries, __VA_ARGS__)         \
