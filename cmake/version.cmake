7e0ef97d5bf8399b637df085203dba2b8cf71827
eugene <eugene.kobyakov@netfoundry.io>
2024-05-31 11:58:15 -0400

    make sure we can build with pre-release (or any) tags such as 2.0.0-alpha1, 2.0.0-rc4, etc

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,48 +1,49 @@
+macro(do_git out_var err_var)
+    execute_process(
+            COMMAND "git" ${ARGN}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            OUTPUT_VARIABLE git_out
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            ERROR_VARIABLE git_err
+            ECHO_ERROR_VARIABLE
+    )
+    if (git_err)
+        set(${err_var} ${git_err})
+        unset(${out_var})
+    else ()
+        set(${out_var} "${git_out}")
+        unset(${err_var})
+    endif ()
+endmacro(do_git)
 
 function(get_version version_file version_var branch_var commit_var)
-    execute_process(
-            COMMAND git describe --tags --match=[0-9]* HEAD
-            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-            OUTPUT_VARIABLE GIT_INFO
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-            ERROR_VARIABLE git_error
-    )
-
-    if (NOT git_error)
-        execute_process(
-                COMMAND git rev-parse --abbrev-ref HEAD
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-                OUTPUT_VARIABLE branch
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-
-        # Get the latest abbreviated commit hash of the working branch
-        execute_process(
-                COMMAND git log -1 --format=%h
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-                OUTPUT_VARIABLE commit
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-
-    endif ()
-
-    if (git_error)
+    do_git(GIT_INFO GIT_ERROR describe --always)
+    if (GIT_ERROR)
         unset(GIT_INFO)
-        message("getting version from ${version_file}")
+        message(NOTICE "getting version from ${version_file}")
         file(STRINGS ${version_file} ver_info)
         list(LENGTH ver_info ver_info_len)
         list(GET ver_info 0 GIT_INFO)
-        list(GET ver_info 1 branch)
-        list(GET ver_info 2 commit)
+        list(GET ver_info 1 _branch)
+        list(GET ver_info 2 _commit)
+        set(${version_var} ${GIT_INFO} PARENT_SCOPE)
+        set(${branch_var} ${_branch} PARENT_SCOPE)
+        set(${commit_var} ${_commit} PARENT_SCOPE)
+        return()
     endif ()
 
-    if (${GIT_INFO} MATCHES "^v?([0-9]+\\.[0-9]+\\.[0-9]+)$")
-        set(_ver ${CMAKE_MATCH_1})
-    elseif (${GIT_INFO} MATCHES "^v?([0-9]+\\.[0-9]+\\.[0-9]+)-([0-9]+)-[^-]*")
-        string(JOIN "." _ver ${CMAKE_MATCH_1} ${CMAKE_MATCH_2})
+    do_git(_branch GIT_ERROR rev-parse --abbrev-ref HEAD)
+    do_git(GIT_INFO GIT_ERROR describe --tags --long --first-parent HEAD)
+    if (${GIT_INFO} MATCHES "^(.*)-([0-9]+)-([^-]*)") # <closest tag>-<distance>-<commit>
+        set(_commit ${CMAKE_MATCH_3})
+        if (${CMAKE_MATCH_2} EQUAL "0") #exact tag
+            set(_ver ${CMAKE_MATCH_1})
+        else ()
+            string(JOIN "." _ver ${CMAKE_MATCH_1} ${CMAKE_MATCH_2})
+        endif ()
     endif ()
 
     set(${version_var} ${_ver} PARENT_SCOPE)
-    set(${branch_var} ${branch} PARENT_SCOPE)
-    set(${commit_var} ${commit} PARENT_SCOPE)
+    set(${branch_var} ${_branch} PARENT_SCOPE)
+    set(${commit_var} ${_commit} PARENT_SCOPE)
 endfunction()
