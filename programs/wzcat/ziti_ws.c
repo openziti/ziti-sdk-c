d9eca7c5d1e20e1bd4c665be7f2acee205fbf111
eugene <eugene.kobyakov@netfoundry.io>
2024-08-17 11:04:35 -0400

    cleanup wzcat sample

--- programs/wzcat/ziti_ws.c
+++ programs/wzcat/ziti_ws.c
@@ -15,16 +15,29 @@
 
 #include <uv.h>
 #include <ziti/ziti.h>
+#include <ziti/ziti_log.h>
 #include <ziti/ziti_src.h>
 #include <tlsuv/websocket.h>
-#include "../../inc_internal/utils.h"
-#include "../../inc_internal/zt_internal.h"
+
+#include <stdio.h>
+
+#define NEWP(var,t) t* var = calloc(1, sizeof(t))
+
+static struct app_ctx {
+    const char *service;
+    uv_loop_t *l;
+} appCtx;
 
 static void init_cb(ziti_context ztx, const ziti_event_t *ev);
 
 int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <config> <service>", argv[0]);
+        exit(1);
+    }
+
     const char *config = argv[1];
-    const char *ws_service = argv[2];
+    appCtx.service = argv[2];
 
     uv_loop_t *l = uv_loop_new();
     ziti_config cfg;
@@ -38,9 +51,10 @@ fprintf(stderr, "ERROR: %s", ziti_errorstr(err)); \
 exit(err);\
 }}while(0)
 
-    check(ziti_load_config(&cfg, argv[1]));
+    check(ziti_load_config(&cfg, config));
     check(ziti_context_init(&ztx, &cfg));
     check(ziti_context_set_options(ztx, &(ziti_options){
+            .app_ctx = &appCtx,
             .event_cb = init_cb,
             .events = ZitiContextEvent,
     }));
@@ -101,13 +115,13 @@ static void on_ws_data(uv_stream_t *s, ssize_t len, const uv_buf_t *buf) {
 }
 
 static void init_cb(ziti_context ztx, const ziti_event_t *ev) {
-    const char *service = ziti_app_ctx(ztx);
+    struct app_ctx *ctx = ziti_app_ctx(ztx);
     NEWP(src, tlsuv_src_t);
-    ziti_src_init(ztx->loop, src, service, ztx);
+    ziti_src_init(ctx->l, src, ctx->service, ztx);
 
     NEWP(ws, tlsuv_websocket_t);
     ws->data = ztx;
-    tlsuv_websocket_init_with_src(ztx->loop, ws, src);
+    tlsuv_websocket_init_with_src(ctx->l, ws, src);
 
     NEWP(connr, uv_connect_t);
     connr->data = ws;
