1ce049f1f315d126ec604c8d7f8576bf0c699c05
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2025-01-17 15:24:46 -0500

    if ZITI_DEBUG is defined, change the log len to much longer and allow debugging the http responses from the controller

--- library/ziti_ctrl.c
+++ library/ziti_ctrl.c
@@ -430,6 +430,9 @@ static void ctrl_body_cb(tlsuv_http_req_t *req, char *b, ssize_t len) {
     if (len > 0) {
         if (resp->resp_content == ctrl_content_json) {
             if (resp->content == NULL) {
+#ifdef ZITI_DEBUG
+                CTRL_LOG(DEBUG, "HTTP RESPONSE: %s", b);
+#endif
                 resp->content = json_tokener_parse_ex(resp->content_proc, b, (int) len);
                 if (resp->content == NULL && json_tokener_get_error(resp->content_proc) != json_tokener_continue) {
                     CTRL_LOG(WARN, "parsing error: %s",
