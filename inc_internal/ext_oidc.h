15a4c9990caa22b9a7aa06840f11a0b2f2d5cd75
eugene <eugene.kobyakov@netfoundry.io>
2026-04-15 16:52:55 -0400

    enhance external OIDC token refresh
    better delay handling

--- inc_internal/ext_oidc.h
+++ inc_internal/ext_oidc.h
@@ -1,18 +1,18 @@
+// Copyright (c) 2026.  NetFoundry Inc
 //
-// 	Copyright NetFoundry Inc.
+// SPDX-License-Identifier: Apache-2.0
 //
-// 	Licensed under the Apache License, Version 2.0 (the "License");
-// 	you may not use this file except in compliance with the License.
-// 	You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// 	https://www.apache.org/licenses/LICENSE-2.0
+// https://www.apache.org/licenses/LICENSE-2.0
 //
-// 	Unless required by applicable law or agreed to in writing, software
-// 	distributed under the License is distributed on an "AS IS" BASIS,
-// 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 	See the License for the specific language governing permissions and
-// 	limitations under the License.
-//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef ZITI_EXT_OIDC_H
 #define ZITI_EXT_OIDC_H
@@ -20,6 +20,7 @@
 #include <uv.h>
 #include <ziti/ziti_model.h>
 #include "tlsuv/http.h"
+#include "jwt.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -53,6 +54,12 @@ struct ext_oidc_client_s {
     void *config;
     void *tokens;
     const char *refresh_grant;
+
+    zt_jwt current;
+    zt_jwt refresh_token;
+    int64_t refresh_token_exp;
+    int refresh_failures;
+    tlsuv_http_req_t *refresh_req;
 
     uv_timer_t *timer;
     char *jwt_token_auth;
